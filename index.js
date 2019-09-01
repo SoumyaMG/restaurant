09f4c1e9fe4417cdc80c6edb8d8b967393482844
Soumya <soumyagidd96@gmail.com>
2019-09-01 13:53:08 +0530

    changed package.json file

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const mongoose = require('./config/database')
 const router = require('./config/routes')
+const path = require('path')
 const app = express()
-const port = 3005
+const port = process.env.PORT || 3005
 
+app.use(express.static(path.join(__dirname, "client/build")))
 app.unsubscribe(express.json())
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"))
+})
 app.use('/', router)
 
 app.listen(port, function () {
